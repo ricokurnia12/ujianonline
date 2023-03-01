1cc069947feec28cef92f19df201b0924bf6df71
bafir24 <bafirgenius@gmail.com>
2023-03-01 15:25:46 +0700

    flex on image and add local storage

--- src/TesTKD.js
+++ src/TesTKD.js
@@ -8,18 +8,41 @@ const Halamansoal = () => {
   // useState Pagination
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [answer, setAnswer] = useState([]);
+  const storageKey = 'jawaban';
+  // jANGAN DIHAPUS
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+  // ------------------
 
   useEffect(() => {
     fetchData();
+    // cek apakah jawaban tersimpan di localStorage dan memuatnya jika ada
+    const storedJawaban = localStorage.getItem(storageKey);
+    if (storedJawaban) {
+      setJawaban(JSON.parse(storedJawaban));
+    } else {
+      setJawaban(Array(soal.length).fill(''));
+    }
   }, []);
+  useEffect(() => {
+    // simpan jawaban ke localStorage setiap kali berubah
+    localStorage.setItem(storageKey, JSON.stringify(jawaban));
+  }, [jawaban]);
 
   // Fetch Data dari API
-
+  // JANGAN DIHAPUS
+  // const fetchData = async () => {
+  //   const response = await fetch('http://localhost:5000/soal');
+  //   const data = await response.json();
+  //   setSoal(data);
+  //   setJawaban(Array(data.length).fill(''));
+  // };
+  // --------------
   const fetchData = async () => {
     const response = await fetch('http://localhost:5000/soal');
     const data = await response.json();
     setSoal(data);
-    setJawaban(Array(data.length).fill(''));
   };
 
   // Function untuk merubah jawaban
@@ -28,17 +51,27 @@ const Halamansoal = () => {
     const newJawaban = [...jawaban];
     newJawaban[currentQuestion] = value;
     setJawaban(newJawaban);
+    console.log(
+      `Jawaban pada soal nomor ${
+        currentQuestion + 1
+      } adalah: ${value}`
+    );
   };
 
   // fUNCTIION SUBMIT JAWABAN
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   const updatedAnswer = [...answer];
-  //   updatedAnswer[currentQuestion] =
-  //     jawaban[`question-${currentQuestion}`] || '';
-  //   setAnswer(updatedAnswer);
-  //   setJawaban({ ...jawaban, [`question-${currentQuestion}`]: '' });
-  // };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const updatedAnswer = [...answer];
+    updatedAnswer[currentQuestion] =
+      jawaban[`question-${currentQuestion}`] || '';
+    setAnswer(updatedAnswer);
+    setJawaban({ ...jawaban, [`question-${currentQuestion}`]: '' });
+    console.log(
+      `Jawaban pada soal nomor ${currentQuestion + 1} adalah: ${
+        jawaban[currentQuestion]
+      }`
+    );
+  };
 
   // function untuk ke halaman sebelumnya
   const handlePrev = () => {
@@ -89,7 +122,7 @@ const Halamansoal = () => {
                     <b>Jenis Tes:</b> {soal[currentQuestion].jenis}
                   </p>
                   <b>Nomor {currentQuestion + 1} :</b>
-
+                  {/* 
                   <div className="kotak-soal px-2 py-2">
                     {soal[currentQuestion].tipe_soal === 'teks' ? (
                       <p>{soal[currentQuestion].pertanyaan}</p>
@@ -97,35 +130,76 @@ const Halamansoal = () => {
                       <img
                         src={soal[currentQuestion].pertanyaan}
                         alt="gambar soal"
+                        className="img-fluid"
+                        style={{
+                          width: '50%',
+                          maxWidth: '500px',
+                          marginBottom: '50px',
+                        }}
+                      />
+                    )}  */}
+                  <div className="kotak-soal px-2 py-2">
+                    {soal[currentQuestion].tipe_soal === 'teks' ? (
+                      <p>{soal[currentQuestion].pertanyaan}</p>
+                    ) : soal[currentQuestion].pertanyaan ===
+                      '' ? null : (
+                      <img
+                        src={soal[currentQuestion].pertanyaan}
+                        alt="gambar soal"
+                        className="img-fluid"
+                        style={{
+                          width: '30%',
+                          maxWidth: '500px',
+                          marginBottom: '50px',
+                        }}
                       />
                     )}
-                    {soal[currentQuestion].jawaban.map(
-                      (option, optionIndex) => (
-                        <div key={optionIndex}>
-                          <input
-                            type="radio"
-                            id={`option-${optionIndex}`}
-                            name={`jawaban-${currentQuestion}`}
-                            value={option}
-                            checked={
-                              jawaban[currentQuestion] === option
-                            }
-                            onChange={handleJawabanChange}
-                          />
-                          <label htmlFor={`option-${optionIndex}`}>
-                            {soal[currentQuestion].tipe_soal ===
-                            'teks' ? (
-                              option
-                            ) : (
-                              <img
-                                src={option}
-                                alt="gambar jawaban"
-                              />
-                            )}
-                          </label>
-                        </div>
-                      )
-                    )}
+                    <div
+                      className={`${
+                        soal[currentQuestion].tipe_soal === 'gambar'
+                          ? 'd-flex'
+                          : ''
+                      }`}
+                    >
+                      {soal[currentQuestion].jawaban.map(
+                        (option, optionIndex) => (
+                          <div key={optionIndex}>
+                            <input
+                              type="radio"
+                              id={`option-${optionIndex}`}
+                              name={`jawaban-${currentQuestion}`}
+                              value={option}
+                              checked={
+                                jawaban[currentQuestion] === option
+                              }
+                              onChange={handleJawabanChange}
+                            />
+                            <label htmlFor={`option-${optionIndex}`}>
+                              {soal[currentQuestion].tipe_soal ===
+                              'teks' ? (
+                                option
+                              ) : (
+                                <div>
+                                  dfdf
+                                  <img
+                                    src={option}
+                                    alt="gambar jawaban"
+                                    className="img-fluid"
+                                    style={{
+                                      width: '20%',
+                                      maxWidth: '400px',
+                                      display: 'inline',
+
+                                      marginBottom: '50px',
+                                    }}
+                                  />
+                                </div>
+                              )}
+                            </label>
+                          </div>
+                        )
+                      )}
+                    </div>
                   </div>
                 </div>
                 <div className="d-flex justify-content-between align-items-center mt-2">
@@ -176,7 +250,11 @@ const Halamansoal = () => {
                 ))}
               </div>
             </div>
-            <button type="submit" className="btn btn-primary">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              // onClick={handleSubmit}
+            >
               Submit
             </button>
           </div>
